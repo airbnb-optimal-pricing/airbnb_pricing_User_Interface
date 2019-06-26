c5618f57accc326045b9114893f2736993e50b79
bharti3bk <bharti3bk@gmail.com>
2019-06-26 14:33:08 -0400

    added responsive section

---
+++ Menu/menu.js
@@ -0,0 +1,9 @@
+
+const toggleMenu = () => {
+    menu.classList.toggle('menu--open');
+  } 
+  
+  const menu = document.querySelector('.menu');
+  const menuButton = document.querySelector('.menu-button');
+    menuButton.addEventListener('click' , toggleMenu  );
+    console.log(menuButton);
